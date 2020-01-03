917423c6af21c162ea7f3eba083043937bab03cd
Alexander Semenov <semenov.ap.87@gmail.com>
2020-01-03 20:35:11 +0300

    test(util): additional checks for printing, file generation and etc.

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -7,25 +7,30 @@ import {
   getData,
   getProtocol,
   pascalCase,
-  rmdir
+  rmdir,
+  showError,
+  showReport,
+  wrap
 } from './utils';
 
 describe('utils', () => {
 
+  let file: File;
+  let dir: Directory<File>;
   const output = resolve(__dirname, Date.now().toString());
 
-  const file: File = {
-    name: 'Bar',
-    file: 'bar.test',
-    template: 'model'
-  };
+  beforeEach(() => {
+    dir = {
+      directory: 'foo',
+      content: []
+    };
 
-  const dir: Directory<File> = {
-    directory: 'foo',
-    content: [
-      file
-    ]
-  };
+    file = {
+      name: 'Bar',
+      file: 'bar.test',
+      template: 'model'
+    };
+  });
 
   it('convert any string to PascalCase', () => {
     expect(pascalCase('foo BAR-Baz')).toBe('FooBarBaz');
@@ -35,9 +40,44 @@ describe('utils', () => {
     expect(await getData(resolve(__dirname, '../fixture.test.json'))).toHaveProperty('swagger');
   });
 
+  it('should throw exception about invalid json data', async () => {
+    await expect(getData(resolve(__dirname, '../LICENSE'))).rejects.toThrowError();
+  });
+
+  it('should try to get file via http protocol', async () => {
+    await expect(getData('foo')).rejects.toThrowError();
+  });
+
+  it('should do request to get a file via https', async () => {
+    jest.mock('./utils');
+    // await getData('https://foo');
+    await expect(getData('https://foo')).rejects.toThrowError();
+    jest.clearAllMocks();
+  });
+
+  it('should not generate a file without data', () => {
+    generate(output, null);
+    expect(existsSync(resolve(output, dir.directory, `${file.file}.ts`))).toBeFalsy();
+  });
+
+  it('should not generate a file without content', () => {
+    generate(output, dir);
+    expect(existsSync(resolve(output, dir.directory, `${file.file}.ts`))).toBeFalsy();
+  });
+
   it('generate file by template to output directory', () => {
+    dir.content.push(file);
     generate(output, dir);
     expect(existsSync(resolve(output, dir.directory, `${file.file}.ts`))).toBeTruthy();
+  });
+
+  it('generate file by template to output subdirectory', () => {
+    dir.content.push({
+      directory: 'bar',
+      content: [file]
+    });
+    generate(output, dir);
+    expect(existsSync(resolve(output, dir.directory, 'bar', `${file.file}.ts`))).toBeTruthy();
   });
 
   it('check index file should be exists', () => {
@@ -49,12 +89,49 @@ describe('utils', () => {
     expect(existsSync(output)).toBeFalsy();
   });
 
+  it('cannot clean output directory', () => {
+    const path = resolve(process.cwd(), 'foo');
+    rmdir('foo');
+    expect(existsSync(path)).toBeFalsy();
+  });
+
+  it('should create and remove relative directory', () => {
+    const name = 'foo';
+    const path = resolve(process.cwd(), name);
+
+    dir.content.push(file);
+    generate(name, dir);
+    expect(existsSync(path)).toBeTruthy();
+
+    rmdir(name);
+    expect(existsSync(path)).toBeFalsy();
+  });
+
   it('return protocol by url', () => {
     expect(getProtocol('https://foo.bar')).toEqual('https');
   });
 
   it('return undefined by wrong url', () => {
     expect(getProtocol('foo.bar')).toBeUndefined();
+  });
+
+  it('should wrap data to directory object', () => {
+    expect(wrap([file], 'foo')).toEqual({
+      directory: 'foo',
+      content: [file]
+    });
+  });
+
+  it('should show error message', () => {
+    spyOn(console, 'error');
+    showError('foo');
+    expect(console.error).toBeCalled();
+  });
+
+  it('should show report', () => {
+    spyOn(console, 'table');
+    showReport(0, 0, 0);
+    expect(console.table).toBeCalled();
   });
 
 });
