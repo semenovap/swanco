582eeb526a3d34125558b0f5ed0175217e00c029
Alexander Semenov <semenov.ap.87@gmail.com>
2023-03-13 18:45:53 +0000

    build: switch from yargs to commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,21 @@
 #!/usr/bin/env node
 
-import {scriptName} from 'yargs';
+import { Command } from 'commander';
 import swanco = require('./index');
 import {showError} from './utils';
 
-const options = scriptName('swanco')
-  .options({
-    'input': {
-      alias: 'i',
-      describe: 'URL or local path to the file with specification in JSON format',
-      demandOption: true
-    },
-    'output': {
-      alias: 'o',
-      default: 'src/app/api',
-      describe: 'Path to write generated content'
-    },
-    'auth': {
-      describe: 'Basic authentication credentials, like "user:password"'
-    },
-    'skip-services': {
-      boolean: false,
-      describe: 'Do not create services content'
-    },
-    'skip-module': {
-      boolean: false,
-      describe: 'Do not create module file'
-    },
-    'hide-report': {
-      boolean: false,
-      describe: 'Hide results of files generation'
-    }
-  })
-  .showHelpOnFail(true)
-  .help()
-  .argv;
+const program = new Command();
 
-swanco(options as any).catch(err => showError(err.message));
+program.name('swanco')
+  .description('This project is a small tool, which generates an Angular module from a Swagger 2.0 specification.')
+  .requiredOption('-i, --input <string>', 'URL or local path to the file with specification in JSON format')
+  .option('-o, --output <string>', 'Path to write generated content')
+  .option('--auth <string>', 'Basic authentication credentials, like "user:password"')
+  .option('--skip-services', 'Do not create services content')
+  .option('--skip-module', 'Do not create module file')
+  .option('--hide-report', 'Hide results of files generation')
+  .showHelpAfterError()
+  .version('4.0.7')
+  .parse();
+
+swanco(program.opts()).catch(err => showError(err.message));
